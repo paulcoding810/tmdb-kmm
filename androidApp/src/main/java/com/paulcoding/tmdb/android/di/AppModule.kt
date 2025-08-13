2cd9cffb8962173ab710ed3f529413f6c88b27e7
paulcoding810 <longnghia2.00@gmail.com>
2025-08-13 23:11:25 +0700

    fix view models DI

--- androidApp/src/main/java/com/paulcoding/tmdb/android/di/AppModule.kt
+++ androidApp/src/main/java/com/paulcoding/tmdb/android/di/AppModule.kt
@@ -2,10 +2,10 @@ package com.paulcoding.tmdb.android.di
 
 import com.paulcoding.tmdb.android.ui.viewmodels.MovieDetailViewModel
 import com.paulcoding.tmdb.android.ui.viewmodels.MovieListViewModel
-import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    singleOf(::MovieDetailViewModel)
-    singleOf(::MovieListViewModel)
+    viewModelOf(::MovieDetailViewModel)
+    viewModelOf(::MovieListViewModel)
 }
